b7e56bec1a80d6413128f14e242bc351d1658fa7
Dennis Glasberg <dennis.glasberg@gmail.com>
2014-03-27 23:52:13 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Dota 2 - Leaderboards</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Dota 2 Leaderboars with some modifications" />
+    <meta name="keywords" content="dota 2, dota, 2, leaderboards, leader boards, leader, boards" />
+</head>
+<body>
+        <h1>Dota 2 Leaderboards</h1>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Rank</th>
+                        <th>Player</th>
+                        <th>Solo MMR</th>
+                    </tr>
+                </thead>
+                <tbody id="leaderboard_body"></tbody>
+            </table>
+            <div id="leaderboard_status"></div>
+        </div>
+    </div>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="js/leaderboards.js"></script>
+</body>
+</html>
